3b3c12d1d0c08ce89fd2aef649c029a12b572fc0
Art Navsegda <artnavsegda@gmail.com>
2016-06-04 00:58:23 +0000

    backend cgi bin

---
+++ back/engine.bin.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	fd = open("/dev/i2c-1",O_RDWR);
+	if (fd<=1)
+	{
+		printf("error on device\n");
+		exit(1);
+	}
+	int addr = 0x09;
+	if (ioctl(fd,I2C_SLAVE,addr) < 0)
+	{
+		printf("ioctl error\n");
+		exit(1);
+	}
+	printf("Content-type: text/plain\n\n");
+	printf("%d\n",i2c_smbus_read_byte_data(fd,0x00));
+	close(fd);
+	return 0;
+}
