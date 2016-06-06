35483545725650962061f645d12437b6bb3e79c0
Art Navsegda <artnavsegda@gmail.com>
2016-06-06 08:41:48 +0300

    timeseries

--- front/graph.js
+++ front/graph.js
@@ -1,5 +1,8 @@
+var data = new TimeSeries();
+
 function createTimeline()
 {
   var chart = new SmoothieChart();
+  chart.addTimeSeries(data);
   chart.streamTo(document.getElementById("chart"));
 }
