6b73206559b1e1d1b4742ba4a7b21303c6ba38be
Art Navsegda <artnavsegda@gmail.com>
2016-06-06 09:23:44 +0000

    wild frontier

--- front/graph.js
+++ front/graph.js
@@ -3,14 +3,15 @@ var xhttp = new XMLHttpRequest();
 
 function createTimeline()
 {
-  var chart = new SmoothieChart();
+  var chart = new SmoothieChart({millisPerPixel:100,maxValue:7000,minValue:5000,grid:{millisPerLine:10000,verticalSections:10,strokeStyle:'#000000'}});
   chart.addTimeSeries(data);
   chart.streamTo(document.getElementById("chart"));
 }
 
 function myFunction()
 {
-  xhttp.open("GET", "cgi-bin/engine.bin?0", false);
+	xhttp.open("GET", "cgi-bin/engine.bin?9", false);
 	xhttp.send();
-  data.append(new Date().getTime(), xhttp.responseText);
+	document.getElementById("demo").innerHTML = xhttp.responseText;
+	data.append(new Date().getTime(), xhttp.responseText);
 }
