434db41deaa9aa187e28a8a925e9ee7a88d2c292
Art Navsegda <artnavsegda@gmail.com>
2016-06-07 18:02:56 +0300

    fill

--- front/graph.js
+++ front/graph.js
@@ -1,5 +1,6 @@
 var data = new TimeSeries();
 var xhttp = new XMLHttpRequest();
+var dogbbq = [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
 
 function createTimeline()
 {
@@ -13,4 +14,12 @@ function myFunction()
   xhttp.open("GET", "cgi-bin/engine.bin", false);
 	xhttp.send();
   data.append(new Date().getTime(), xhttp.responseText);
+}
+
+function myFunction2()
+{
+  for (i = 0; i < 20; i++)
+  {
+    data.append(new Date().getTime()-(i*1000), dogbbq[i]);
+  }
 }
